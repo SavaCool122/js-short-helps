fbd1141652252efb1e11bf69d79332396b1ba855
SavaCool122 <mrsavacool@gmail.com>
2019-09-27 13:53:19 +0300

    add more patterns v2

---
+++ bahaivro/chain_of_responsibility.js
@@ -0,0 +1,40 @@
+class MySum {
+  constructor(initialValue = 42) {
+    this.sum = initialValue;
+  }
+
+  add(value) {
+    this.sum += value;
+    return this;
+  }
+}
+
+// ВОзращает обьект из-за this из-за этого можно добавлять методы
+// const sum1 = new MySum()
+// console.log(sum1.)
+
+const sum1 = new MySum();
+console.log(
+  sum1
+    .add(8)
+    .add(10)
+    .add(1)
+    .add(9).sum
+);
+
+const sum1 = new MySum();
+console.log(
+  sum1
+    .add(8)
+    .add(10)
+    .add(1)
+    .add(9).sum
+);
+
+const sum2 = new MySum(0);
+console.log(
+  sum2
+    .add(1)
+    .add(2)
+    .add(3).sum
+);
