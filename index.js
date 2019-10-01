c45b6ee2ed0935526b65ef09331b2a94d51385c4
SavaCool122 <46084440+SavaCool122@users.noreply.github.com>
2019-10-01 13:58:17 +0300

    help for short code JS

---
+++ index.js
@@ -0,0 +1,65 @@
+//
+// const num = 42;
+// let result;
+
+// BAD
+
+// if (num > 20) {
+//   result = 'More than 20';
+// } else {
+//   result = 'Less than 20';
+// }
+
+//GOOD
+
+// const result = num > 20 ? 'More than 20' : 'Less than 20';
+
+// console.log(result);
+
+//###############
+
+// let varible;
+// let varible2;
+
+//BAD
+
+// if (varible) {
+//   varible2 = varible;
+// } else {
+//   varible2 = 'some value';
+// }
+
+//GOOD
+
+// varible2 = varible || 'some value';
+
+//##########
+//bad
+// let x
+// let y
+// let z = 42
+
+// Good//
+
+// let x,y,z = 42
+
+//#######
+
+// const names = ['Igor', 'Olga', 'Elena'];
+
+// for (let name of names) {
+//   console.log(name);
+//   // names in log
+// }
+
+// for (index in names) {
+//   console.log(index);
+//   //index in log
+// }
+// names.forEach((el, index, array) => {
+//   console.log(array);
+// // });
+
+// console.log(2 ** 3);
+// console.log(2 ** 4);
+// console.log(2 ** 5);
