c4fc204443f4945d328d47e13464b5c67a2385fe
SavaCool122 <46084440+SavaCool122@users.noreply.github.com>
2019-10-08 12:51:52 +0300

    Add arrays methods

---
+++ arrays methods/find.js
@@ -0,0 +1,16 @@
+const items = [
+  { name: 'Bike', price: 100 },
+  { name: 'TV', price: 200 },
+  { name: 'Album', price: 10 },
+  { name: 'Book', price: 12 },
+  { name: 'Phone', price: 500 },
+  { name: 'Computer', price: 1000 },
+  { name: 'Keyboard', price: 25 }
+];
+
+const foundItem = items.find((item) => {
+  return item.name === 'Book';
+});
+
+console.log(foundItem);
+//log a item with name book
---
+++ arrays methods/forEach.js
@@ -0,0 +1,16 @@
+const items = [
+  { name: 'Bike', price: 100 },
+  { name: 'TV', price: 200 },
+  { name: 'Album', price: 10 },
+  { name: 'Book', price: 12 },
+  { name: 'Phone', price: 500 },
+  { name: 'Computer', price: 1000 },
+  { name: 'Keyboard', price: 25 }
+];
+
+items.forEach((item) => {
+  console.log(item.name);
+  //log all names
+});
+
+//log a filtred array
