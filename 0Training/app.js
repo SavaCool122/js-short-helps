c5a6de2da132bb6df24616d68b6e6ec202aaf176
SavaCool122 <46084440+SavaCool122@users.noreply.github.com>
2019-10-16 14:07:44 +0300

    add more code

--- 0Training/app.js
+++ 0Training/app.js
@@ -1,1 +1,30 @@
 // First function to rewrite from <lib>.js
+
+var express = require('express');
+const fs = require('fs');
+const router = express.Router();
+const app = express();
+
+// Home page route.
+app.get('/', function(req, res) {
+  res.send('Wiki home page');
+});
+
+// About page route.
+app.get('/about', function(req, res) {
+  res.send('About this wiki');
+});
+
+module.exports = router;
+
+// app.post('/users', function(req, res) {
+//   const user = req.body;
+//   fs.appendToFile(
+//     'users.txt',
+//     JSON.stringify({ name: user.name, age: user.age }),
+//     (err) => {
+//       res.send('successfully registered');
+//     }
+//   );
+// });
+app.listen(3000);
