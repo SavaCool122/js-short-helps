ecdb24e4f4520c09abe467927f09f9c556e42c1e
SavaCool122 <46084440+SavaCool122@users.noreply.github.com>
2019-10-17 15:48:01 +0300

    Update set.js

--- map and set/set.js
+++ map and set/set.js
@@ -1,6 +1,6 @@
 const set = new Set([1, 2, 4, 4, 5, 6, 6, 7, 7, 8, 8, 8, 9, 9]);
 
-console.log(set); // losg is { 1, 2, 4, 5, 6, 7, 8, 9 }
+console.log(set); // logs is { 1, 2, 4, 5, 6, 7, 8, 9 }
 
 setArray = [...set];
 
